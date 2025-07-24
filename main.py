f8036a2496c6b8ef11819a35f2a4ffef09f67f2e
MH <ethan.yeh.chic@gmail.com>
2025-07-24 23:16:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ def get_db():
         yield db
     finally:
         db.close()
-def hash_password(password: str):
-    return pwd_context.hash(password)
+#def hash_password(password: str):
+#    return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
