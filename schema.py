dab6e3519b40e39b4422cddd66bd56d9cb140acf
MH <ethan.yeh.chic@gmail.com>
2025-07-24 22:59:47 +0800

    Update schema.py

--- schema.py
+++ schema.py
@@ -11,13 +11,13 @@ class LoginRequest(BaseModel):
     password: str
 
 # models.py
-class UserDB(Base):
-    __tablename__ = "main_accounts"
-    account_id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(100), unique=True, index=True)
-    pwd = Column(String(128))  # hashed password
-    status = Column(SmallInteger, default=9)
-    create_at = Column(DateTime, default=datetime.utcnow)
+#class UserDB(Base):
+#    __tablename__ = "main_accounts"
+#    account_id = Column(Integer, primary_key=True, index=True)
+#    email = Column(String(100), unique=True, index=True)
+#    pwd = Column(String(128))  # hashed password
+#    status = Column(SmallInteger, default=9)
+#    create_at = Column(DateTime, default=datetime.utcnow)
 
 class Token(BaseModel):
     access_token: str
