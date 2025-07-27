399d9d00ec55fe1fa25d1e65522929ef5c20c6ac
MH <ethan.yeh.chic@gmail.com>
2025-07-27 12:19:13 +0800

    Update models.py

--- models.py
+++ models.py
@@ -47,3 +47,28 @@ class GameUserMappingDB(Base):
     game_elapse = Column(SmallInteger)  
     flag = Column(Boolean, default=False)
 
+# models.py
+class Wager(Base):
+    __tablename__ = "wager"
+    w_id = Column(Integer, primary_key=True,Integer,index=True, autoincrement=True)
+    w_date_time = Column(DateTime)
+    bet_type = Column(String(50))
+    p_id = Column(SmallInteger,ForeignKey("providers.provider_id"))
+    game_id = Column(SmallInteger,ForeignKey("games.game_id"))
+    account_id = Column(SmallInteger,ForeignKey("accounts.account_id"))
+    bet_flag = Column(Boolean)
+
+
+class WagerItem(Base):
+    __tablename__ = "wager_item"
+    w_id = Column(Integer,ForeignKey("wager.w_id"))
+    wager_item_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    e_date_time = Column(DateTime)
+    ball = Column(String(64))
+    status = Column(SmallInteger)
+    win_loss = Column(SmallInteger)
+    bet_amount = Column(Integer)
+    win_amount = Column(Integer)
+    deduct_amount = Column(Integer)
+
+
