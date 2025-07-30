7f55e3e144c7e0fc277a32eb7dc70c3f2f8181d0
MH <ethan.yeh.chic@gmail.com>
2025-07-30 19:26:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -283,9 +283,9 @@ def get_usernames(provider_id: int = Query(...),game_name: str = Query(...),db: 
     if not game:
         raise HTTPException(status_code=404, detail="Game not found")
 
-    usernames = db.query(GameAccountDB.username).filter(GameAccountDB.provider_id == game.provider_id).all()
+    usernames = db.query(GameAccountDB.username,GameAccountDB.game_elapse).filter(GameAccountDB.provider_id == game.provider_id).all()
 
-    return {"usernames": [u.username for u in usernames]}
+    return {"usernames": [u.username,u.game_elapse for u in usernames]}
 @app.get("/api/get-username-time")
 def get_ausername_time(provider_id: int,game_name: str,db: Session = Depends(get_db)):
     results = db.query(GameAccountDB.username, GameAccountDB.game_elapse).filter(
